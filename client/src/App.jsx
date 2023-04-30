3ca55c33502b77b9c8eae3157baa82db2a82794a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-30 06:53:32 +0200

    web structure

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "boxicons";
+
+function App() {
+  return (
+    <div className="App">
+      <i class="bx bxl-facebook-circle"></i>
+      <i class="bx bxl-github"></i>
+      <i class="bx bx-up-arrow-alt"></i>
+      <i class="bx bxs-phone"></i>
+      <i class="bx bx-envelope"></i>
+      <i class="bx bxs-user"></i>
+      <i class="bx bxl-facebook-square"></i>
+    </div>
+  );
+}
+
+export default App;
